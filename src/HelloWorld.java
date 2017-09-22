1135759c053a2be20347f3aa2c85af604696082d
feng <1548861381@qq.com>
2017-09-22 17:41:10 +0800

    add a new class

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by feng on 2017/9/22.
+ */
+public class HelloWorld {
+    String string;
+    public void say(String string){
+        System.out.println("The result is:"+string);
+    }
+}
